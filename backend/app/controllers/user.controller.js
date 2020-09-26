bdef30fbba88479f59f149598c1c3376ed759fa4
anasselbaz0 <anasselbaz0@gmail.com>
2020-09-26 10:28:44 +0100

    [Back] add controllers

---
+++ backend/app/controllers/user.controller.js
@@ -0,0 +1,15 @@
+exports.allAccess = (req, res) => {
+    res.status(200).send("Public Content.");
+};
+//
+// exports.private1Board = (req, res) => {
+//     res.status(200).send("private1 Content.");
+// };
+//
+// exports.private2Board = (req, res) => {
+//     res.status(200).send("private2 Content.");
+// };
+//
+// exports.private3Board = (req, res) => {
+//     res.status(200).send("private3 Content.");
+// };
